9956ac637dca08220667a1e81035dc17ff8f1012
Helpingakp <99520904+Helpingakp@users.noreply.github.com>
2023-04-10 17:08:01 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -109,7 +109,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="🌹 Join Channel 🌹", url=f"{SUPPORT_CHANNEL}""),
+            InlineKeyboardButton(text="🌹 Join Channel 🌹", url=f"{SUPPORT_CHANNEL}"),
         ],
         [
             InlineKeyboardButton(
